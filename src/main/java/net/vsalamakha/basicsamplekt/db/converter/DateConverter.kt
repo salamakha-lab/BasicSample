fc08b2a8688787b584fdacea98c865d7b1280049
Valery Salamakha <v.salamakha@gmail.com>
2019-02-28 15:43:31 +1100

    Comment deleted

--- src/main/java/net/vsalamakha/basicsamplekt/db/converter/DateConverter.kt
+++ src/main/java/net/vsalamakha/basicsamplekt/db/converter/DateConverter.kt
@@ -31,17 +31,3 @@ open class DateConverter {
         return date?.time
     }
 }
-
-/*
-object DateConverter {
-    @TypeConverter
-    fun toDate(timestamp: Long?): Date? {
-        return if (timestamp == null) null else Date(timestamp)
-    }
-
-    @TypeConverter
-    fun toTimestamp(date: Date?): Long? {
-        return date?.time
-    }
-}
-*/
